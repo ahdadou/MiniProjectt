07a4343f3f08942fa32ac457d15499660cf2dad0
ahdadou <ibra.ahdadou@gmail.com>
2021-03-14 20:47:38 +0100

    tesst

--- MiniProject/Form1.cs
+++ MiniProject/Form1.cs
@@ -11,7 +11,7 @@ namespace MiniProject
 {
     public partial class Form1 : Form
     {
-        
+        //tessst
         static Form1 _obj;
         private Button currentBtn;
         private Panel leftBorderBtn;
